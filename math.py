68cee1135de8ef332b1f1cd30ee574b899ca9e95
EmsCode82 <emsgithub@gmail.com>
2021-11-01 19:39:50 -0500

    .

---
+++ math.py
@@ -0,0 +1,28 @@
+operand1 = 65
+operand2 = 83.22
+
+#Addition
+print (operand1 + operand2)
+
+#Subtraction
+print(operand1 - operand2)
+
+#Multiplication
+print(operand1 * operand2)
+
+#Division
+print(operand1 / operand2)
+
+##print(9 % 3)
+##print(10 % 3)
+##print(15% 6)
+
+print(16 % 2) #even
+print(17 % 2) #odd
+
+print(3 ** 3) #exponent
+print(10 ** 2) #exponent
+
+print(10 // 3) #Floor Division
+print(15 // 6) #Floor Division
+
